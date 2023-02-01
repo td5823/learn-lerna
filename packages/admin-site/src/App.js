f7f72e6190cafa40cd1fe3af94db4073a4bfe2b3
tianzhengdong <582361831@qq.com>
2023-02-01 09:43:54 +0800

    2

--- packages/admin-site/src/App.js
+++ packages/admin-site/src/App.js
@@ -5,7 +5,7 @@ import WaterForm from '@mono-repo-demo/common/components/WaterForm'
 function App() {
   return (
     <div className="App">
-     {'撒打算打算的 '}
+     {'撒打算打算的 222'}
       <WaterForm />
       {'222 111'}
     </div>
