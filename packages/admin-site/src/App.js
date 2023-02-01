e8b26abc09cd12088ece22d2e1f567c8b9154434
tianzhengdong <582361831@qq.com>
2023-02-01 09:25:09 +0800

    lerna

---
+++ packages/admin-site/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import WaterForm from '@mono-repo-demo/common/components/WaterForm'
+
+function App() {
+  return (
+    <div className="App">
+     {'撒打算打算的'}
+      <WaterForm />
+    </div>
+  );
+}
+
+export default App;
